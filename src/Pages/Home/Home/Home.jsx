3e7c6f5beb4a4c55c429c8636639276166710903
Tarek Aziz <mdaziz3289@gmail.com>
2023-07-23 03:54:56 +0600

    first commit

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,14 @@
+import CollegeGallery from "../CollegeGallery/CollegeGallery";
+import Research from "../Research/Research";
+
+const Home = () => {
+    return (
+        <div>
+            <h2>this is home</h2>
+            <CollegeGallery></CollegeGallery>
+            <Research></Research>
+        </div>
+    );
+};
+
+export default Home;
